d0ec80b8bdc536e14efe36a2d46bc001152540f3
Cesar Ferracin <cesarferracin@gmail.com>
2023-09-25 16:48:43 +0100

    feat: home page completed

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,23 +1,26 @@
-import { Block, Button } from 'vcc-ui';
+import { Flex, Button, Text, Spacer } from 'vcc-ui';
 import { useCars } from '../hooks/useCars';
 import { CarCard } from './CarCard';
+import styles from '../../public/css/Home.module.css'
 
 export const HelloWorld: React.FC = () => {
 
   const cars = useCars()
-  console.log(cars)
 
   return (
-    <Block extend={{ padding: 20 }}>
-      {cars.map(car => {
-        return (
-          <CarCard 
-            key={car.id} 
-            car={car}
-          />
-        )
-      })}
-      <Button>Click me!</Button>
-    </Block>
+    <div className={styles.homeWrapper}>
+      <Text variant='cook'>Todos os modelos Recharge</Text>
+      <Spacer/>
+      <div className={styles.cardsWrapper}>
+        {cars.map(car => {
+          return (
+            <CarCard 
+              key={car.id} 
+              car={car}
+            />
+          )
+        })}
+      </div>
+    </div>
   );
 };
